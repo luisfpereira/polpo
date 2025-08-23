e1c3437357d5747ceb36957c08a8ea0f2687419f
Luís F. Pereira <luisfgper@gmail.com>
2025-08-23 00:02:56 -0700

    Update dash app tests

--- tests/polpo/dash/test_app.py
+++ tests/polpo/dash/test_app.py
@@ -1,29 +1,79 @@
+import time
+
 import pytest
 
+# NB: tests if everything is created, nothing else
 
-# TODO: adapt here, do not want all the combinations
-@pytest.mark.parametrize("colorized", [False])
-@pytest.mark.parametrize("hormones", [True])
-@pytest.mark.parametrize("week", [True])
-@pytest.mark.parametrize("overlay", [False])
-@pytest.mark.parametrize("hideable", [False])
-# @pytest.mark.parametrize("data", ["hipp", "maternal", "multiple"])
-@pytest.mark.parametrize("data", ["hipp"])
+
+def _test_app(dash_duo, app, maximize_window=True, sleep=2):
+    # TODO: control these two parameters with env vars
+    dash_duo.start_server(app)
+
+    if maximize_window:
+        dash_duo.driver.maximize_window()
+
+    # FIXME: because something weird with wait_for_page
+    time.sleep(sleep)
+
+    dash_duo.wait_for_page(timeout=3)
+
+    assert dash_duo.get_logs() == [], "Browser console has errors"
+
+
+@pytest.mark.parametrize(
+    "data,hideable,overlay,week,hormones,colorized",
+    [
+        ("hipp", False, True, True, False, False),
+        ("hipp", False, False, True, True, True),
+        ("maternal", False, True, True, True, False),
+        ("multiple", True, False, True, False, False),
+        ("multiple", False, False, True, True, True),
+    ],
+)
 def test_mesh_explorer(dash_duo, data, hideable, overlay, week, hormones, colorized):
     from polpo.dash.app.mesh_explorer import my_app
 
-    dash_duo.start_server(
-        my_app(data, hideable, overlay, week, hormones, colorized, run=False)
+    _test_app(
+        dash_duo,
+        my_app(data, hideable, overlay, week, hormones, colorized, run=False),
     )
-    dash_duo.wait_for_page(timeout=3)
-    assert dash_duo.get_logs() == [], "Browser console has errors"
 
 
-@pytest.mark.parametrize("column", [True, False])
-@pytest.mark.parametrize("swapped", [True, False])
-def test_image_app(dash_duo, column, swapped):
+@pytest.mark.parametrize("switchable", [False, True])
+@pytest.mark.parametrize("graph_first", [False, True])
+@pytest.mark.parametrize("as_col", [False, True])
+@pytest.mark.parametrize("session_view", [False, True])
+def test_mri_app(dash_duo, session_view, as_col, graph_first, switchable):
+    if not switchable:
+        from polpo.dash.app.mri_explorer import my_app
+    else:
+        from polpo.dash.app.switchable_mri_explorer import my_app
+
+    _test_app(
+        dash_duo,
+        my_app(session_view, as_col, graph_first, run=False),
+    )
+
+
+@pytest.mark.parametrize("image_first", [True, False])
+@pytest.mark.parametrize("as_col", [True, False])
+def test_image_app(dash_duo, as_col, image_first):
     from polpo.dash.app.image_explorer import my_app
 
-    dash_duo.start_server(my_app(column, swapped, run=False))
-    dash_duo.wait_for_page(timeout=3)
-    assert dash_duo.get_logs() == [], "Browser console has errors"
+    _test_app(
+        dash_duo,
+        my_app(as_col, image_first, run=False),
+    )
+
+
+# def test_multi_image_app(dash_duo):
+#     from polpo.dash.app.multi_image_explorer import my_app
+
+#     dash_duo.start_server(my_app(run=False))
+
+#     # FIXME: because something weird with wait_for_page
+#     time.sleep(1)
+
+#     dash_duo.wait_for_page(timeout=3)
+
+#     assert dash_duo.get_logs() == [], "Browser console has errors"
